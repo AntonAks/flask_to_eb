74599bcc967b764caf13e8852ddd9841393163a1
AntonAks <antonaks84@gmail.com>
2021-12-23 11:18:40 +0200

    minor change

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from typing import Mapping
 from flask import Flask
 app = Flask(__name__)
 
@@ -5,3 +6,6 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return "Hello World"
+
+if __name__ == "__main__":
+    app.run()
